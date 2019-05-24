aac4800d923d9c1946a2bee9ab273ca0f65abe46
Jens Schiller <jens.schiller@jat-gmbh.de>
2019-05-24 16:55:38 +0200

    various changes

--- Pizza/Form1.cs
+++ Pizza/Form1.cs
@@ -18,6 +18,8 @@ namespace Pizza
     {
         private List<Order> OrdersLst = new List<Order>();
 
+        private List<Order> OrdersLstTmp = new List<Order>();
+
         // nur für Testzwecke
         private List<CustomerProps> CustomerPropsLst = new List<CustomerProps>();
         private List<CustomerControl> CustomerCrtlsLst = new List<CustomerControl>();
@@ -36,44 +38,70 @@ namespace Pizza
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //CustomerPropsLst.Add(new CustomerProps("Anton Abendroth", "-"));
-            //CustomerPropsLst.Add(new CustomerProps("Berta Bertram", "-"));
-            //CustomerPropsLst.Add(new CustomerProps("Christel", "-"));
-            //CustomerPropsLst.Add(new CustomerProps("Kanzler", "-"));
-            //CustomerPropsLst.Add(new CustomerProps("Schilli", "-"));
+            // create test xml file
+        
+            CustomerProps props1 = new CustomerProps();
+            props1.CustomerName = "Test1";
 
-            //OrdersLst.Add(new Order(CustomerPropsLst));
+            CustomerProps props2 = new CustomerProps();
+            props2.CustomerName = "Test2";
 
-            //int i = -20;
+            CustomerProps props3 = new CustomerProps();
+            props3.CustomerName = "Test3";
 
-            //foreach (CustomerProps tmp in CustomerPropsLst)
-            //{
-            //    CustomerControl customerControl = new CustomerControl(tmp);
-            //    groupBox2.Controls.Add(customerControl);
-            //    customerControl.Location = new Point(5, i += 40);
-            //}
+            CustomerProps props4 = new CustomerProps();
+            props4.CustomerName = "Test4";
 
-            //SaveOrdersToXml();
-        }
+            CustomerProps props5 = new CustomerProps();
+            props5.CustomerName = "Test5";
 
-        void SaveOrdersToXml()
-        {
+            List<CustomerProps> propsLst = new List<CustomerProps>();
+
+            propsLst.Add(props1);
+            propsLst.Add(props2);
+            propsLst.Add(props3);
+            propsLst.Add(props4);
+            propsLst.Add(props5);
+
+            Order orderTmp1 = new Order();
+            orderTmp1.OrderTimestamp = DateTime.Parse("2019-04-01T18:45:32");
+            orderTmp1.CustomerPropsLst = propsLst;
+            orderTmp1.OrderTitle = "Order 1";
+
+            Order orderTmp2 = new Order();
+            orderTmp2.OrderTimestamp = DateTime.Parse("2019-07-01T18:45:32");
+            orderTmp2.CustomerPropsLst = propsLst;
+
+            orderTmp2.OrderTitle = "Order 2";
+
+            Order orderTmp3 = new Order();
+            orderTmp3.OrderTimestamp = DateTime.Parse("2019-11-01T18:45:32");
+            orderTmp3.CustomerPropsLst = propsLst;
+
+            orderTmp3.OrderTitle = "Order 3";
+
+            OrdersLstTmp.Add(orderTmp1);
+            OrdersLstTmp.Add(orderTmp2);
+            OrdersLstTmp.Add(orderTmp3);
+
+
             serializer = new XmlSerializer(typeof(List<Order>));
             fileStream = new FileStream(@".\orders.xml", FileMode.Create);
-            serializer.Serialize(fileStream, OrdersLst);
+            serializer.Serialize(fileStream, OrdersLstTmp);
             fileStream.Close();
+
         }
 
         // load orders from xml file
         void LoadOrders()
         {
             string filePath = ".\\orders.xml";
-            StreamReader fileStream;
+            FileStream fileStream;
             XmlSerializer deserializer = new XmlSerializer(typeof(List<Order>));
 
             try
             {
-                fileStream = new StreamReader(filePath);
+                fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
                 OrdersLst = (List<Order>)deserializer.Deserialize(fileStream);
                 fileStream.Close();
             }
@@ -86,7 +114,7 @@ namespace Pizza
                     using (OpenFileDialog openFileDialog = new OpenFileDialog())
                     {
                         openFileDialog.InitialDirectory = @".\";
-                        openFileDialog.Filter = "|orders.xml";
+                        openFileDialog.Filter = "|*.xml";
                         openFileDialog.FilterIndex = 1;
                         openFileDialog.RestoreDirectory = true;
 
@@ -95,10 +123,10 @@ namespace Pizza
                             // Get the path of specified file
                             filePath = openFileDialog.FileName;
 
-                            //// Read the contents of the file into a fileStream
-                            //fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-                            //OrdersLst = (List<Order>)deserializer.Deserialize(fileStream);
-                            //fileStream.Close();
+                            // Read the contents of the file into a fileStream
+                            fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                            OrdersLst = (List<Order>)deserializer.Deserialize(fileStream);
+                            fileStream.Close();
                         }
                         else
                         {
@@ -116,35 +144,38 @@ namespace Pizza
             
             foreach(Order i in OrdersLst)
             {
-                lst.Add(i.OrderTimestamp.ToString("yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo) + " " + i.OrderTitle);
+                lst.Add(i.OrderTimestamp.ToString("yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo) + " " + i.OrderTitle); // sort orders by timestamp
             }
 
-            cboOrders.Items.AddRange(lst.OrderBy(x => x).ToArray());
+            cboOrders.Items.AddRange(lst.OrderBy(x => x).ToArray()); // add sorted orders to combo box
          
             if(cboOrders.Items.Count > 0)
             {
-                cboOrders.SelectedIndex = cboOrders.Items.Count - 1;
+                cboOrders.SelectedIndex = cboOrders.Items.Count - 1; // set last order as active
             }
         }
 
         // generate table with empty order by using the customers of the last order
         void InitOrderTable()
         {
-            Order order = new Order();
-            order = OrdersLst.OrderBy(x => x.OrderTimestamp).Last() as Order;
-            int i = -20;
-
-            foreach(CustomerProps customerProps in order.CustomerPropsLst)
+            if (OrdersLst.Any())
             {
-                CustomerControl customerControl = new CustomerControl(customerProps);
-                CustomerCrtlsLst.Add(customerControl);
-                groupBox2.Controls.Add(customerControl);
-                customerControl.Location = new Point(5, i += 40);
-            }
+                Order order = new Order();
+                order = OrdersLst.OrderBy(x => x.OrderTimestamp).Last() as Order;
+                int i = -20;
 
-            if(order.OrderClosed)
-            {
-                groupBox2.Enabled = false;
+                foreach (CustomerProps customerProps in order.CustomerPropsLst)
+                {
+                    CustomerControl customerControl = new CustomerControl(customerProps);
+                    CustomerCrtlsLst.Add(customerControl);
+                    groupBox2.Controls.Add(customerControl);
+                    customerControl.Location = new Point(5, i += 40);
+                }
+
+                if (order.OrderClosed)
+                {
+                    groupBox2.Enabled = false;
+                } 
             }
         }
     }
